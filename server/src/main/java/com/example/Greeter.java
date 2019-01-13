663535567abb0be02e5a76167e41f271bf0e8a5a
giannguyen <khonggian2007@gmail.com>
2019-01-13 23:34:30 +0700

    final checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,18 +5,18 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+    /**
+     * This is a constructor.
+     */
+    public Greeter() {
 
-  }
+    }
 
-  /**
-   * @param someone this is the parameter
-   * @return 		greeting message
-   */
-  public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+    /**
+     * @param someone this is the parameter
+     * @return greeting message
+     */
+    public final String greet(final String someone) {
+        return String.format("Hello, %s!", someone);
+    }
 }
